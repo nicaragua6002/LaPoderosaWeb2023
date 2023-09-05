3ea74970bcb3095c5c299cc91e42652b39e2290c
MLB22 <MLB22@LMB09>
2023-09-05 12:52:19 -0600

    Agregar archivos de proyecto.

---
+++ LaPoderosaWeb/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LaPoderosaWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Información de contacto";
+            ViewBag.Fecha = DateTime.Today.ToString();
+            return View();
+        }
+    }
+}
---
+++ LaPoderosaWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LaPoderosaWeb.MvcApplication" Language="C#" %>
